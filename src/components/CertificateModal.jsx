ea5003e20a24d08e5d6d61e8fb585a47bc4d6dab
Mauricio Quiroga <mauriquiroga97@gmail.com>
2026-02-18 17:33:22 -0300

    Actualizacion de  estilos para mejorar el responsive

--- src/components/CertificateModal.jsx
+++ src/components/CertificateModal.jsx
@@ -1,6 +1,16 @@
 import { createPortal } from "react-dom";
+import { useEffect } from "react";
 
 const CertificateModal = ({ isOpen, onClose, certPath, certTitle }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("modal-landscape");
+    } else {
+      document.body.classList.remove("modal-landscape");
+    }
+    return () => document.body.classList.remove("modal-landscape");
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return createPortal(
@@ -8,17 +18,10 @@ const CertificateModal = ({ isOpen, onClose, certPath, certTitle }) => {
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <div className="modal-header">
           <h3>{certTitle}</h3>
-          <button className="modal-close" onClick={onClose}>
-            [ X ]
-          </button>
+          <button className="modal-close" onClick={onClose}>[ X ]</button>
         </div>
         <div className="modal-body">
-          <iframe
-            src={`${certPath}#toolbar=0&zoom=page-width`}
-            title={certTitle}
-            width="100%"
-            height="100%"
-          />
+          <iframe src={`${certPath}#toolbar=0&zoom=page-width`} title={certTitle} />
         </div>
       </div>
     </div>,
